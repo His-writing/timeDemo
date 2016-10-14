3c0b455a459e853119d1ed60f26c742e61e851f1
树振郭 <shuzhenguo@macBook-Pro.local>
2016-10-14 15:48:01 +0800

    Initial Commit

---
+++ timeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  timeDemo
+//
+//  Created by shuzhenguo on 2016/10/14.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ timeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  timeDemo
+//
+//  Created by shuzhenguo on 2016/10/14.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
